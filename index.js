650c6c7e1c6a0d5a80375acc1b4f9229dcfac40a
hritikg950 <hritikg950@gmail.com>
2021-07-21 16:50:56 +0530

    heroku deploy

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const profileRoutes = require('./routes/profile')
 const postRoutes = require('./routes/post')
 const app = express()
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
